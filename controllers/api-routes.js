6842032a050adc94c20f1d7ec1bf9bd0d341cafa
adham bishr <adhambishr@gmail.com>
2017-12-20 16:12:21 -0800

    somewhat working

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,8 +1,48 @@
 // Requiring our models
 var db = require("../models/index.js");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
     res.render("index");
+  });
+
+  // A GET route for scraping the cnn website
+  app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with request
+    axios.get("http://www.echojs.com/").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+      //console.log(response.data);
+
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("article h3").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+
+        conosle.log(result);
+
+        // // Create a new Article using the `result` object built from scraping
+        // db.Article.create(result)
+        //   .then(function(dbArticle) {
+        //     // If we were able to successfully scrape and save an Article, send a message to the client
+        //     res.send("Scrape Complete");
+        //   })
+        //   .catch(function(err) {
+        //     // If an error occurred, send it to the client
+        //     res.json(err);
+        //   });
+      });
+    });
   });
 };
