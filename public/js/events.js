4e700ee3b5c30f10b0b50feeb723bb24987a1d14
adham bishr <adhambishr@gmail.com>
2017-12-22 12:37:58 -0800

    working color for save button

--- public/js/events.js
+++ public/js/events.js
@@ -1,2 +1,11 @@
 console.log("JS Loaded");
 
+$(document).ready(function() {
+  $("button[type=save]").on("click", function(event) {
+    event.preventDefault();
+    console.log("clicked");
+    console.log(this);
+    $(this).css("background-color", "green");
+    $(this).css("color", "white");
+  });
+});
